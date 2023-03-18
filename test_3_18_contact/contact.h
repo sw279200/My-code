d1e5accb20bc20065c72f81fa4f3424fbfb4fbc4
sw_mypro <2792069459@qq.com>
2023-03-18 22:01:53 +0800

    通讯录的实现

---
+++ test_3_18_contact/contact.h
@@ -0,0 +1,45 @@
+#pragma once
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#define MAX_NAME 40
+#define MAX_SEX 20
+#define MAX_TELE 30
+#define MAX_ADDR 50
+#define MAX_DATA 1000
+//定义一个结构体存放通讯录联系人信息
+typedef struct PeoInfo{
+	char name[MAX_NAME];
+	char sex[MAX_SEX];
+	int age;
+	char tele[MAX_TELE];
+	char addr[MAX_ADDR];
+}PeoInfo;
+
+//通讯录
+typedef struct Contact {
+	PeoInfo data[MAX_DATA];
+	int sz;
+}Contact;
+
+
+//初始化通讯录
+void InitContact(Contact * pc);
+
+//添加指定联系人
+void AddContact(Contact* pc);
+
+//删除指定联系人
+void DelContact(Contact* pc);
+
+//打印联系人信息
+void PrintContact(Contact* pc);
+
+//查找指定联系人
+void SearchContact(Contact* pc);
+
+//修改指定联系人信息
+void ModifyContact(Contact* pc);
+
+//对通讯录进行排序
+void SortContact(Contact* pc);
