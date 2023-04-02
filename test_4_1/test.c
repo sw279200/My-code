daca6095fa1c0f6a98332aa65df1baad7e4c67b8
sw_mypro <2792069459@qq.com>
2023-04-02 19:24:29 +0800

    c语言文件操作(2)

---
+++ test_4_1/test.c
@@ -0,0 +1,211 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+////二进制的写文件 - fwrite
+//#include<stdio.h>
+//struct S
+//{
+//	char name[20];
+//	int age;
+//	float score;
+//};
+//int main()
+//{
+//	struct S s = { "pengge",18,99.9f };
+//	//打开文件
+//	FILE* pf = fopen("test.dat", "wb");
+//	if (pf == NULL)
+//	{
+//		perror("fopen");
+//		return 1;
+//	}
+//	//读文件
+//	fwrite(&s, sizeof(struct S), 1, pf);
+//	//关闭文件
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+
+////fread - 读文件
+//#include<stdio.h>
+//struct S
+//{
+//	char name[20];
+//	int age;
+//	float score;
+//};
+//int main()
+//{
+//	struct S s = {0};
+//	//打开文件
+//	FILE* pf = fopen("test.dat", "rb");
+//	if (pf == NULL)
+//	{
+//		perror("fopen");
+//		return 1;
+//	}
+//	//读文件
+//	fread(&s, sizeof(struct S), 1, pf);
+//	printf("%s %d %f\n", s.name, s.age, s.score);
+//	//关闭文件
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//int main()
+//{
+//	//打开文件
+//	FILE* pf = fopen("test.dat", "r");
+//	if (pf == NULL)
+//	{
+//		perror("fopen");
+//		return 1;
+//	}
+//	//读文件
+//	int ch = fgetc(pf);
+//	printf("%c\n", ch);//p
+//	ch = fgetc(pf);
+//	printf("%c\n", ch);//e
+//	ch = fgetc(pf);
+//	printf("%c\n", ch);//n
+//	ch = fgetc(pf);
+//	printf("%c\n", ch);//g
+//
+//	//fseek(pf, -3, SEEK_CUR);
+//	fseek(pf, 1, SEEK_SET);
+//	ch = fgetc(pf);
+//	printf("%c\n", ch);//希望读到的是e
+//	//关闭文件
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+//#include<stdio.h>
+//
+//int main()
+//{
+//	//打开文件
+//	FILE* pf = fopen("test.dat", "r");
+//	if (pf == NULL)
+//	{
+//		perror("fopen");
+//		return 1;
+//	}
+//	//读文件
+//	int ch = fgetc(pf);
+//	printf("%c\n", ch);//p
+//	ch = fgetc(pf);
+//	printf("%c\n", ch);//e
+//	ch = fgetc(pf);
+//	printf("%c\n", ch);//n
+//	ch = fgetc(pf);
+//	printf("%c\n", ch);//g
+//
+//	rewind(pf);
+//	printf("%d\n", ftell(pf));
+//	ch = fgetc(pf);
+//	printf("%c\n", ch);//p
+//	//关闭文件
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 10000;
+//	FILE* pf = fopen("test.txt", "wb");
+//	fwrite(&a, 4, 1, pf);//二进制的形式写到文件中
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//#include <stdlib.h>
+//int main()
+//{
+//	FILE* pf = fopen("test.txt", "r");
+//	if (pf == NULL)
+//	{
+//		return 1;
+//	}
+//	//读文件
+//	int ch = 0;
+//	while ((ch = fgetc(pf)) != EOF)
+//	{
+//		printf("%c ", ch);
+//	}
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//#include <windows.h>
+//
+////VS2013 WIN10环境测试
+//int main()
+//{
+//	FILE* pf = fopen("test.txt", "w");
+//	fputs("abcdef", pf);//先将代码放在输出缓冲区
+//	printf("睡眠10秒-已经写数据了，打开test.txt文件，发现文件没有内容\n");
+//	Sleep(10000);
+//	printf("刷新缓冲区\n");
+//	fflush(pf);//刷新缓冲区时，才将输出缓冲区的数据写到文件（磁盘）
+//	//注：fflush 在高版本的VS上不能使用了
+//	printf("再睡眠10秒-此时，再次打开test.txt文件，文件有内容了\n");
+//	Sleep(10000);
+//	fclose(pf);
+//	//注：fclose在关闭文件的时候，也会刷新缓冲区
+//	pf = NULL;
+//	return 0;
+//}
+//
+//
+//
+
+#include<stdio.h>
+int main()
+{
+	//实现一个代码，拷贝一个文件
+	//打开文件
+	//打开被读的文件
+	FILE* pfRead = fopen("test.txt", "r");
+	if (pfRead == NULL)
+	{
+		perror("open file for read");
+		return 1;
+	}
+	//打开要写的文件
+	FILE* pfWrite = fopen("test2.txt", "w");
+	if (pfWrite == NULL)
+	{
+		fclose(pfRead);
+		pfRead = NULL;
+		pfRead = NULL;
+		perror("open file for write");
+		return 1;
+	}
+
+	//拷贝
+	int ch = 0;
+	while ((ch=fgetc(pfRead)) != EOF)
+	{
+		fputc(ch,pfWrite);
+	}
+
+	//关闭文件
+	fclose(pfRead);
+	pfRead = NULL;
+	fclose(pfWrite);
+	pfWrite = NULL;
+	return 0;
+}
