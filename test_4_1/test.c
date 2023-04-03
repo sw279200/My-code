4610265402b08aae9a93adcf0fc07e82129b68ba
sw_mypro <2792069459@qq.com>
2023-04-04 00:06:25 +0800

    c语言文件操作（2）

--- test_4_1/test.c
+++ test_4_1/test.c
@@ -148,64 +148,64 @@
 //}
 
 
-//#include <stdio.h>
-//#include <windows.h>
-//
-////VS2013 WIN10环境测试
-//int main()
-//{
-//	FILE* pf = fopen("test.txt", "w");
-//	fputs("abcdef", pf);//先将代码放在输出缓冲区
-//	printf("睡眠10秒-已经写数据了，打开test.txt文件，发现文件没有内容\n");
-//	Sleep(10000);
-//	printf("刷新缓冲区\n");
-//	fflush(pf);//刷新缓冲区时，才将输出缓冲区的数据写到文件（磁盘）
-//	//注：fflush 在高版本的VS上不能使用了
-//	printf("再睡眠10秒-此时，再次打开test.txt文件，文件有内容了\n");
-//	Sleep(10000);
-//	fclose(pf);
-//	//注：fclose在关闭文件的时候，也会刷新缓冲区
-//	pf = NULL;
-//	return 0;
-//}
-//
-//
-//
+#include <stdio.h>
+#include <windows.h>
 
-#include<stdio.h>
+//VS2013 WIN10环境测试
 int main()
 {
-	//实现一个代码，拷贝一个文件
-	//打开文件
-	//打开被读的文件
-	FILE* pfRead = fopen("test.txt", "r");
-	if (pfRead == NULL)
-	{
-		perror("open file for read");
-		return 1;
-	}
-	//打开要写的文件
-	FILE* pfWrite = fopen("test2.txt", "w");
-	if (pfWrite == NULL)
-	{
-		fclose(pfRead);
-		pfRead = NULL;
-		pfRead = NULL;
-		perror("open file for write");
-		return 1;
-	}
-
-	//拷贝
-	int ch = 0;
-	while ((ch=fgetc(pfRead)) != EOF)
-	{
-		fputc(ch,pfWrite);
-	}
-
-	//关闭文件
-	fclose(pfRead);
-	pfRead = NULL;
-	fclose(pfWrite);
-	pfWrite = NULL;
+	FILE* pf = fopen("test.txt", "w");
+	fputs("abcdef", pf);//先将代码放在输出缓冲区
+	printf("睡眠10秒-已经写数据了，打开test.txt文件，发现文件没有内容\n");
+	Sleep(10000);
+	printf("刷新缓冲区\n");
+	fflush(pf);//刷新缓冲区时，才将输出缓冲区的数据写到文件（磁盘）
+	//注：fflush 在高版本的VS上不能使用了
+	printf("再睡眠10秒-此时，再次打开test.txt文件，文件有内容了\n");
+	Sleep(10000);
+	fclose(pf);
+	//注：fclose在关闭文件的时候，也会刷新缓冲区
+	pf = NULL;
 	return 0;
 }
+//
+//
+//
+
+//#include<stdio.h>
+//int main()
+//{
+//	//实现一个代码，拷贝一个文件
+//	//打开文件
+//	//打开被读的文件
+//	FILE* pfRead = fopen("test.txt", "r");
+//	if (pfRead == NULL)
+//	{
+//		perror("open file for read");
+//		return 1;
+//	}
+//	//打开要写的文件
+//	FILE* pfWrite = fopen("test2.txt", "w");
+//	if (pfWrite == NULL)
+//	{
+//		fclose(pfRead);
+//		pfRead = NULL;
+//		pfRead = NULL;
+//		perror("open file for write");
+//		return 1;
+//	}
+//
+//	//拷贝
+//	int ch = 0;
+//	while ((ch=fgetc(pfRead)) != EOF)
+//	{
+//		fputc(ch,pfWrite);
+//	}
+//
+//	//关闭文件
+//	fclose(pfRead);
+//	pfRead = NULL;
+//	fclose(pfWrite);
+//	pfWrite = NULL;
+//	return 0;
+//}
