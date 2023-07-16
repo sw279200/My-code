d38c49c7c9d6384a2b300f78dfc65a13f0604862
sw_mypro <2792069459@qq.com>
2023-07-16 22:08:58 +0800

    冒泡排序，选择排序

---
+++ test_7_15/sort.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+#include <stdlib.h>
+void PrintArray(int* a, int n);
+
+void Swap(int* a, int* b);
+
+void InsertSort(int* a, int n);
+
+void ShellSort(int* a, int n);
+
+void BubbleSort(int* a, int n);
+
+void SelectSort(int* a, int n);
