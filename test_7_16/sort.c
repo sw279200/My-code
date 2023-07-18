48be33d797f0a3bb85f2c4c16e69f47f551f880d
sw_mypro <2792069459@qq.com>
2023-07-18 09:25:55 +0800

    堆排序

---
+++ test_7_16/sort.c
@@ -0,0 +1,172 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "sort.h"
+
+void Swap(int* a, int* b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void PrintArray(int* a, int n)
+{
+	for (int i = 0; i < n; i++)
+		printf("%d ", a[i]);
+
+	printf("\n");
+}
+
+//时间复杂度（最坏）：O(N^2) 逆序
+//时间复杂度（最好）：O(N) 有序
+void InsertSort(int* a, int n)
+{
+	for (int i = 1; i < n; i++)
+	{
+		//[0,end]之间有序，插入tmp后依然有序
+		int end = i - 1;
+		int tmp = a[i];
+		while (end >= 0)
+		{
+			if (a[end] > tmp)
+			{
+				a[end + 1] = a[end];
+				end--;
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		a[end + 1] = tmp;
+
+	}
+}
+
+//希尔排序
+void ShellSort(int* a, int n)
+{
+	int group = n;
+	while (group > 1)
+	{
+		group = group / 3 + 1;
+		//对组并排
+		for (int i = 0; i < n - group; i++)
+		{
+			int end = i;
+			int temp = a[i + group];
+			while (end >= 0)
+			{
+				if (a[end] > temp)
+				{
+					a[end + group] = a[end];
+					end -= group;
+				}
+				else
+				{
+					break;
+				}
+			}
+
+			a[end + group] = temp;
+		}
+	}
+
+
+}
+
+void BubbleSort(int* a, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		bool flag = false;
+		for (int j = 1; j < n - i; j++)
+		{
+			if (a[j - 1] > a[j])
+			{
+				flag = true;
+				int tmp = a[j - 1];
+				a[j - 1] = a[j];
+				a[j] = tmp;
+			}
+		}
+
+		if (flag == false)
+			break;
+	}
+}
+
+//选择排序
+void SelectSort(int* a, int n)
+{
+	int begin = 0;
+	int end = n - 1;
+
+	while (begin < end)
+	{
+		int maxi = end;
+		int mini = begin;
+		for (int i = begin; i <= end; i++)
+		{
+			if (a[i] > a[maxi])
+				maxi = i;
+
+			if (a[i] < a[mini])
+				mini = i;
+		}
+
+		Swap(&a[begin], &a[mini]);
+		if (begin == maxi)
+		{
+			maxi = mini;
+		}
+		Swap(&a[end], &a[maxi]);
+
+		begin++;
+		end--;
+	}
+}
+
+
+//向下调整算法
+void AdjustDown(int* a, int n, int parent)
+{
+	int child = parent * 2 + 1;
+	while (child < n)
+	{
+		if (child + 1 < n && a[child + 1] > a[child])
+		{
+			child++;
+		}
+
+		if (a[parent] < a[child])
+		{
+			Swap(&a[parent], &a[child]);
+			parent = child;
+			child = parent * 2 + 1;
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+//堆排序
+void HeapSort(int* a, int n)
+{
+	for (int i = (n - 1 - 1) / 2; i >= 0; i--)
+	{
+		AdjustDown(a, n, i);
+	}
+	
+	int end = n - 1;
+	while (end > 0)
+	{
+		Swap(&a[0], &a[end]);
+		AdjustDown(a, end, 0);
+		--end;
+	}
+
+}
---
+++ test_7_16/sort.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+#include <stdlib.h>
+void PrintArray(int* a, int n);
+
+void Swap(int* a, int* b);
+
+void InsertSort(int* a, int n);
+
+void ShellSort(int* a, int n);
+
+void BubbleSort(int* a, int n);
+
+void SelectSort(int* a, int n);
+
+void AdjustDown(int* a, int n, int parent);
+
+void HeapSort(int* a, int n);
