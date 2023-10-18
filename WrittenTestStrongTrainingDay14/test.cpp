b283df232843de882429ec3845998c9ea83a91ea
sw_mypro <2792069459@qq.com>
2023-10-18 17:34:19 +0800

    第十四次笔试强训

--- WrittenTestStrongTrainingDay14/test.cpp
+++ WrittenTestStrongTrainingDay14/test.cpp
@@ -33,11 +33,30 @@ void solve()
 }
 
 
-
+//пртк╣д╢Эвс
+int  n;
+int arr[1010] = {0};
+int dfs(int index,int sum,int mul)
+{
+    int ret = 0;
+    for (int i = index + 1; i < n; i++)
+    {
+        int Sum = sum + arr[i], Mul = mul * arr[i];
+        if (Sum > Mul) ret += dfs(i, Sum, Mul) + 1;
+        else if (arr[i] == 1) ret += dfs(i, Sum, Mul);
+        else break;
+        Sum -= arr[i], Mul /= mul;
+        while (i < n - 1 && arr[i] == arr[i + 1])i++;
+    }
+    return ret;
+}
 
 int main() {
 
-    solve();
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    sort(arr, arr + n);
+    cout << dfs(0, 1, 1)<<endl;
     return 0;  
 }
 
