a16913ae590dd2e249e7a9029db0e373726adb69
sw_mypro <2792069459@qq.com>
2023-10-27 23:39:32 +0800

    递归搜索回溯算法01

---
+++ Recurrent_search_backtrace/test.cpp
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<stack>
+using namespace std;
+
+class Solution {
+public:
+    //汉诺塔问题
+    void hanota(vector<int>& A, vector<int>& B, vector<int>& C) { 
+        int n = A.size();
+        move(n, A, B, C);
+    } 
+
+    void move(int n, vector<int>& A, vector<int>& B, vector<int>& C)
+    {
+        if (n == 1) {
+            C.push_back(A.back());
+            A.pop_back();
+            return;
+        }
+
+        move(n - 1, A, C, B);    // 将A上面n-1个通过C移到B
+        C.push_back(A.back());  // 将A最后一个移到C
+        A.pop_back();          // 这时，A空了
+        move(n - 1, B, A, C);     // 将B上面n-1个通过空的A移到C
+
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
