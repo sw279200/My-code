43adbf8192e5d8a1e54087d5f8c89666720990d0
sw_mypro <2792069459@qq.com>
2023-10-28 23:14:11 +0800

    合并两个有序链表(递归版)

--- Recurrent_search_backtrace/test.cpp
+++ Recurrent_search_backtrace/test.cpp
@@ -6,6 +6,15 @@
 #include<stack>
 using namespace std;
 
+struct ListNode {
+   int val;
+   ListNode *next;
+   ListNode() : val(0), next(nullptr) {}
+   ListNode(int x) : val(x), next(nullptr) {}
+   ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ 
+
 class Solution {
 public:
     //汉诺塔问题
@@ -27,6 +36,26 @@ public:
         A.pop_back();          // 这时，A空了
         move(n - 1, B, A, C);     // 将B上面n-1个通过空的A移到C
 
+    }
+
+    //合并两个有序链表
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (!list1) return list2;
+        if (!list2) return list1;
+        ListNode* head = nullptr;
+        if (list1->val < list2->val)
+        {
+            head = list1;
+            head->next = mergeTwoLists(list1->next, list2);
+
+        }
+        else
+        {
+            head = list2;
+            head->next = mergeTwoLists(list1, list2->next);
+        }
+
+        return head;
     }
 };
 
