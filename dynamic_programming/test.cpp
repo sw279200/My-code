6285e0c5b43c96be20bc9539fc556fd9097a4884
sw_mypro <2792069459@qq.com>
2023-10-29 22:17:31 +0800

    dp问题：最长数对链

--- dynamic_programming/test.cpp
+++ dynamic_programming/test.cpp
@@ -100,6 +100,21 @@ public:
         }   
         return retcount;
     }
+
+    //最长数对链
+    int findLongestChain(vector<vector<int>>& pairs) {
+        int n = pairs.size();
+        sort(pairs.begin(), pairs.end());
+        vector<int> dp(n, 1);
+        int Max = 1;
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 0; j < i; j++)
+                if (pairs[i][0] > pairs[j][1]) dp[i] = max(dp[i], dp[j] + 1);
+            Max = max(Max, dp[i]);
+        }
+        return Max;
+    }
 };
 
 int main()
