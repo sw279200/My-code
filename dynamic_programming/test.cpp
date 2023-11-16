777dcb3e965accb66eb2235ad6e342cac103a192
sw_mypro <2792069459@qq.com>
2023-11-16 22:57:14 +0800

    等差数列划分

--- dynamic_programming/test.cpp
+++ dynamic_programming/test.cpp
@@ -169,6 +169,32 @@ public:
         }
         return ret;
     }
+
+    //等差数列划分 II - 子序列
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<long long, vector<int>>hash;
+        for (int i = 0; i < n; i++) hash[nums[i]].push_back(i);
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        int sum = 0;
+        for (int j = 2; j < n; j++)
+        {
+            for (int i = 1; i < j; i++)
+            {
+                long long a = (long long)2 * nums[i] - nums[j];
+                if (hash.count(a))
+                {
+                    for (auto k : hash[a])
+                    {
+                        if (k < i) dp[i][j] += dp[k][i] + 1;
+                    }
+                }
+                sum += dp[i][j];
+            }
+        }
+        return sum;
+    }
 };
 
 int main()
