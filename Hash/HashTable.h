a0685914f83561147340aa5bf75b727ae227e647
sw_mypro <2792069459@qq.com>
2023-11-19 21:53:41 +0800

    哈希

--- Hash/HashTable.h
+++ Hash/HashTable.h
@@ -1,7 +1,8 @@
 #pragma once
+#include<vector>
 
-namespace myhash {
-	
+namespace bit
+{
 	enum Status
 	{
 		EMPTY,
@@ -13,34 +14,38 @@ namespace myhash {
 	struct HashData
 	{
 		pair<K, V> _kv;
-		Status _s;
+		Status _s;          //状态
 	};
 
 	template<class K, class V>
 	class HashTable
 	{
 	public:
+		HashTable()
+		{
+			_tables.resize(10);
+		}
 
 		bool Insert(const pair<K, V>& kv)
 		{
-
-			//负载因子0.7就扩容
+			// 负载因子0.7就扩容
 			if (_n * 10 / _tables.size() == 7)
 			{
 				size_t newSize = _tables.size() * 2;
 				HashTable<K, V> newHT;
 				newHT._tables.resize(newSize);
-				//遍历旧表
+				// 遍历旧表
 				for (size_t i = 0; i < _tables.size(); i++)
 				{
 					if (_tables[i]._s == EXIST)
 					{
-						newHT.Insert(_tables._kv);
+						newHT.Insert(_tables[i]._kv);
 					}
 				}
 
-				//交换旧表和新表
+				_tables.swap(newHT._tables);
 			}
+
 			size_t hashi = kv.first % _tables.size();
 			while (_tables[hashi]._s == EXIST)
 			{
@@ -57,9 +62,18 @@ namespace myhash {
 		}
 
 	private:
-		vector<HashData> _tables;
-		//负载因子
-		size_t _n = 0;
+		vector<HashData<K, V>> _tables;
+		size_t _n = 0; // 存储的关键字的个数
 	};
 }
 
+	void TestHT1()
+	{
+		bit::HashTable<int, int> ht;
+		int a[] = { 4,14,24,34,5,7,1 };
+		for (auto e : a)
+		{
+			ht.Insert(make_pair(e, e));
+		}
+		ht.Insert(make_pair(3,3));
+	}
--- Hash/test.cpp
+++ Hash/test.cpp
@@ -3,41 +3,48 @@
 #include<iostream>
 #include<unordered_set>
 #include<unordered_map>
-
 using namespace std;
+#include"HashTable.h"
 
+
+//
+//int main()
+//{
+//	unordered_set<int>s;
+//	s.insert(2);
+//	s.insert(5);
+//	s.insert(1);
+//	s.insert(6);
+//	s.insert(4);
+//	unordered_set<int>::iterator it = s.begin();
+//	while (it != s.end())
+//	{
+//		cout << *it << " ";
+//		it++;
+//	}
+//	cout << endl;
+//
+//	for (auto e : s)
+//	{
+//		cout << e << " ";
+//	}
+//	cout << endl;
+//
+//	unordered_map<string, string>hash;
+//	hash["left"] = "×ó±ß";
+//	hash["right"] = "ÓÒ±ß";
+//	hash["sort"] = "ÅÅĞò";
+//
+//	for (auto& kv : hash)
+//	{
+//		cout << kv.first << ":" << kv.second << endl;
+//	}
+// 
+//	return 0;
+//}
 
 int main()
 {
-	unordered_set<int>s;
-	s.insert(2);
-	s.insert(5);
-	s.insert(1);
-	s.insert(6);
-	s.insert(4);
-	unordered_set<int>::iterator it = s.begin();
-	while (it != s.end())
-	{
-		cout << *it << " ";
-		it++;
-	}
-	cout << endl;
 
-	for (auto e : s)
-	{
-		cout << e << " ";
-	}
-	cout << endl;
-
-	unordered_map<string, string>hash;
-	hash["left"] = "×ó±ß";
-	hash["right"] = "ÓÒ±ß";
-	hash["sort"] = "ÅÅĞò";
-
-	for (auto& kv : hash)
-	{
-		cout << kv.first << ":" << kv.second << endl;
-	}
- 
 	return 0;
 }
