dddf5cb3d2ab1cef575ceb946e98e53fc681738b
@sw_mypro <2792069459@qq.com>
2023-11-29 16:53:33 +0800

    回文子串

--- dynamic_programming/test.cpp
+++ dynamic_programming/test.cpp
@@ -195,10 +195,34 @@ public:
         }
         return sum;
     }
+
+
+    // ╩ьндвс╢╝
+    int countSubstrings(string s) {
+        int n = s.size();
+        vector<vector<int>>dp(n, vector<int>(n));
+        int ret = 0;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int j = i; j < n; j++)
+            {
+                if (s[i] == s[j])
+                {
+                    if (i == j)dp[i][j] = 1;
+                    else if (i + 1 == j)dp[i][j] = 1;
+                    else if (dp[i + 1][j - 1] == 1)dp[i][j] = 1;
+                }
+                ret += dp[i][j];
+            }
+        }
+
+        return ret;
+    }
 };
 
 int main()
 {
-
+    Solution sol;
+    cout << sol.countSubstrings("dnncbwoneinoplypwgbwktmvkoimcooyiwirgbxlcttgteqthcvyoueyftiwgwwxvxvg") << endl;
     return 0;
 }
