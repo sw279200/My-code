8c27252c0e7e6b9370e2bf283ef44e3e23320b2e
@sw_mypro <2792069459@qq.com>
2023-12-13 22:59:18 +0800

    分割回文串II

--- dynamic_programming/test.cpp
+++ dynamic_programming/test.cpp
@@ -291,6 +291,38 @@ public:
                     return true;
         return false;
     }
+
+    //分割回文串II
+    int minCut(string s) {
+        int n = s.size();
+        vector<vector<bool>> isPal(n, vector<bool>(n));
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int j = i; j < n; j++)
+            {
+                if (s[i] == s[j])
+                {
+                    isPal[i][j] = i + 1 < j ? isPal[i + 1][j - 1] : true;
+                }
+            }
+        }
+
+        vector<int> dp(n, INT_MAX);
+        for (int i = 0; i < n; i++)
+        {
+            if (isPal[0][i])dp[i] = 0;
+            else
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if (isPal[j][i])
+                        dp[i] = min(dp[j - 1] + 1, dp[i]);
+                }
+            }
+        }
+
+        return dp[n - 1];
+    }
 };
 
 int main()
